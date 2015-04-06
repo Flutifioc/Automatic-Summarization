085c2b147c95f0bf3ec9e6c2c3ddb36bb8272eaa
lahai <lara.haidar-ahmad@polymtl.ca>
2015-04-06 17:31:58 -0400

    integration du wikipedia parser

--- Automatic_Summarization_Maven/src/main/java/Executables/WordAnnoter.java
+++ Automatic_Summarization_Maven/src/main/java/Executables/WordAnnoter.java
@@ -1,35 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package Executables;
 
-import java.util.ArrayList;
 import java.util.HashSet;
+import edu.jhu.nlp.wikipedia.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Hashtable;
 
-/**
- *
- * @author lufon
- */
 public class WordAnnoter {
 
     public static void main(String[] args) {
         
         Hashtable<String, Double> idf = new Hashtable<String, Double>();
-        HashSet<String> currentDocWords = new HashSet<String>();
         int N = 0;
         
-        String[] documents; // the files pathes 
-        for(int i = 0 ; i < documents.length ; ++i)
-        {
-            String document = documents[i]; // TODO : get the file's path from documents[i], open it and read the document
-            String[] words = document.split(" "); // TODO: remove all ponctuation and capital letter, and get a String[] of the words
+        WikiPageIterator it = initializeWikipediaParser("simplewiki-latest-pages-articles.xml.bz2");
+        
+        String document = "";
+        while (it.hasMorePages()) {
+            document = getWikipediaArticle(it);
+            HashSet<String> currentDocWords = new HashSet<String>();
+            N++;
+            
+            String[] words = document.split(" "); // TODO: remove capital letter
             for(String word : words)
             {
-                N++;
                 if(idf.containsKey(word))
                 {
                     if(!currentDocWords.contains(word))
@@ -41,6 +36,7 @@ public class WordAnnoter {
                 else
                 {
                     idf.put(word, 1.0);
+                    currentDocWords.add(word);
                 }
             }
         }
@@ -50,5 +46,45 @@ public class WordAnnoter {
             idf.put(word, Math.log(N/idf.get(word)));
         }
         
+        writeIDFToTextFile(idf);
+    }
+    
+    private static WikiPageIterator initializeWikipediaParser(String path)
+    {
+        WikiXMLParser wxp = WikiXMLParserFactory.getDOMParser(path);
+        WikiPageIterator it = null;
+        
+        try {
+            wxp.parse();
+            it = wxp.getIterator();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return it;
+    }
+    
+    private static String getWikipediaArticle(WikiPageIterator it)
+    {
+        String text = "";
+        
+        if(it.hasMorePages()) {
+            WikiPage page = it.nextPage();
+            text = page.getText();
+        }
+        
+        return text;
+    }
+    
+    private static void writeIDFToTextFile(Hashtable<String, Double> idf)
+    {
+     try {
+        BufferedWriter out = new BufferedWriter(new FileWriter("idf.txt"));
+            for(String word : idf.keySet()) {
+                out.write(word + " " + idf.get(word) + "\n");
+                out.newLine();
+            }
+            out.close();
+        } catch (IOException e) {}   
     }
 }
